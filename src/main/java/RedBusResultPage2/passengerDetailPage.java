1b233343209beb88ed16fd94e078588fbc1b5cce
DAKSHAD S <dakshaddakshad@gmail.com>
2025-06-26 23:09:21 +0530

    Update passengerDetailPage.java

--- src/main/java/RedBusResultPage2/passengerDetailPage.java
+++ src/main/java/RedBusResultPage2/passengerDetailPage.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-import redbus.abstractComponents.abstractComponents;
 import redbus2.abstractcomponents.abstractComponets2;
 
 public class passengerDetailPage extends abstractComponets2 {
